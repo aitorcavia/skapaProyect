dacfe77da01271ecaf08678b640b9bad10486f63
aitor <aitor@DESKTOP-3BC4VJ0>
2019-10-02 11:50:36 +0200

    Commit inicial

---
+++ skapaProyect/src/skapaProyect/ventanaInicio.java
@@ -0,0 +1,11 @@
+package skapaProyect;
+
+public class ventanaInicio {
+
+	public static void main(String[] args) {
+		String holamundo = "hola Mundo";
+		System.out.println(holamundo);
+
+	}
+
+}
---
+++ skapaProyect/src/skapaProyect/ventanaLogin.java
@@ -0,0 +1,11 @@
+package skapaProyect;
+
+public class ventanaLogin {
+
+	public static void main(String[] args) {
+		String ventanaLogin = "ventanaLogin";
+		System.out.println(ventanaLogin);
+
+	}
+
+}
