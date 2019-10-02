7490cf52e7690b9a7aee77303c8ff208890c7d61
aitor <aitor@DESKTOP-3BC4VJ0>
2019-10-02 12:48:16 +0200

    Inicio ventanaLogin

--- skapaProyect/src/skapaProyect/ventanaLogin.java
+++ skapaProyect/src/skapaProyect/ventanaLogin.java
@@ -1,10 +1,21 @@
 package skapaProyect;
 
-public class ventanaLogin {
+import javax.swing.*;
+
+public class ventanaLogin extends JFrame{
+	
+	private JLabel labelTitulo;
+	private JTextArea textoUsuario;
+	private JTextArea textoContrasenya;
+	private JButton botonInicioSesion;
+	private JLabel textoOlvidoContrasenya;
+	
+	public ventanaLogin() {
+		
+	}
 
 	public static void main(String[] args) {
-		String ventanaLogin = "ventanaLogin";
-		System.out.println(ventanaLogin);
+		new ventanaLogin();
 
 	}
 
