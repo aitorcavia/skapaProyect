b3bd3d8f29bdc5ff8c095d78fd75a02a575b4265
aitor <aitor@DESKTOP-3BC4VJ0>
2019-10-02 11:52:58 +0200

    Commit de prueba

---
+++ skapaProyect/src/skapaProyect/ventanaRegistro.java
@@ -0,0 +1,11 @@
+package skapaProyect;
+
+public class ventanaRegistro {
+
+	public static void main(String[] args) {
+		String ventanaRegistro = "ventanaRegistro";
+		System.out.println(ventanaRegistro);
+
+	}
+
+}
