9998368e1e3d1c6aee30a1ca12028b7d62aafae0
Gaizka <Gaizka@10.164.53.208>
2019-10-04 12:37:37 +0200

    He puesto un syso con tony

--- skapaProyect/src/skapaProyect/ventanaInicio.java
+++ skapaProyect/src/skapaProyect/ventanaInicio.java
@@ -5,6 +5,7 @@ public class ventanaInicio {
 	public static void main(String[] args) {
 		String holamundo = "hola Mundo";
 		System.out.println(holamundo);
+		System.out.println("Tony eres tonto");
 		
 
 	}
