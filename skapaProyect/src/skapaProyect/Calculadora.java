377fcf5eef2311f7aa9924a248f9e039311efe5f
aitor <aitor@DESKTOP-3BC4VJ0>
2019-10-08 08:17:00 +0200

    Actualizacion de Calculadora

--- skapaProyect/src/skapaProyect/Calculadora.java
+++ skapaProyect/src/skapaProyect/Calculadora.java
@@ -1,33 +1,13 @@
 package skapaProyect;
 
-public class Calculadora {
+public interface Calculadora {
 
-	public int sumar (int a, int b) {
-		return a+b;
-	}
+	public int sumar (int a, int b);
 	
-	public int restar (int a, int b) {
-		int solucion;
-		if (a>b) {
-			solucion = a-b;
-		}else {
-			solucion = b-a;
-		}
+	public int restar (int a, int b);
+
+	public int multiplicar (int a, int b) ;
+
+	public boolean esPar (int a);
 		
-		return solucion;
-	}
-	
-	public int multiplicar (int a, int b) {
-		return a*b;
-	}
-	
-	public boolean esPar (int a) {
-		boolean solucion;
-		if (a%2 == 0) {
-			solucion = true;
-		}else {
-			solucion = false;
-		}
-		return solucion;
-	}
 }
