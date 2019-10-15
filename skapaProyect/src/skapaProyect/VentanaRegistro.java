4660339bc71ae33242491a2f42a36053022b87a5
Gaizka <Gaizka@10.164.53.208>
2019-10-15 09:24:04 +0200

    He empezado la VentanaInicio

--- skapaProyect/src/skapaProyect/VentanaRegistro.java
+++ skapaProyect/src/skapaProyect/VentanaRegistro.java
@@ -6,6 +6,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
 
 public class VentanaRegistro extends JFrame {
 
@@ -32,7 +33,7 @@ public class VentanaRegistro extends JFrame {
 	 */
 	public VentanaRegistro() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 478);
+		setBounds(100, 100, 349, 519);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
