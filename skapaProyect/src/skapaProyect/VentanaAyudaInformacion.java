b31b5f18279c6ca405b98bf87d73b6993aa17554
anton <anton@DESKTOP-7D2K253>
2019-10-18 13:41:17 +0200

    Coloreo

--- skapaProyect/src/skapaProyect/VentanaAyudaInformacion.java
+++ skapaProyect/src/skapaProyect/VentanaAyudaInformacion.java
@@ -8,6 +8,7 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JTextPane;
 import java.awt.Font;
+import java.awt.Color;
 
 public class VentanaAyudaInformacion extends JFrame {
 
@@ -36,6 +37,7 @@ public class VentanaAyudaInformacion extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 349, 559);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(135, 206, 250));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
