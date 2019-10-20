90fc8f0998644db5aafd06b7e15f8451cd31ea6f
anton <anton@DESKTOP-7D2K253>
2019-10-20 17:32:41 +0200

    VentanaChat editada

--- skapaProyect/src/skapaProyect/VentanaCliente.java
+++ skapaProyect/src/skapaProyect/VentanaCliente.java
@@ -9,10 +9,18 @@ import java.awt.GridLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 import java.awt.TextArea;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class VentanaCliente  {
 
@@ -22,6 +30,14 @@ public class VentanaCliente  {
 	JTextArea area_chat = null;
 	JPanel contenedor_areachat = null;
 	JPanel contenedor_btntxt = null;
+//	JScrollPane scroll = null;
+	
+	Socket socket = null;
+	BufferedReader lector = null;
+	PrintWriter escritor=null;
+
+
+	
 	
 	public VentanaCliente() {
 		hacerInterfaz();
@@ -33,6 +49,8 @@ public class VentanaCliente  {
 		txt_mensaje = new JTextField(4);
 		contenedor_areachat = new JPanel();
 		contenedor_areachat.setLayout(new GridLayout(1,1));
+	//	contenedor_areachat.add(scroll);
+
 		contenedor_btntxt = new JPanel();
 		contenedor_btntxt.setLayout(new GridLayout(1,2));
 		contenedor_btntxt.add(txt_mensaje);
@@ -40,6 +58,8 @@ public class VentanaCliente  {
 		ventana_chat.getContentPane().setLayout(new BorderLayout());
 		ventana_chat.getContentPane().add(contenedor_areachat,BorderLayout.NORTH);
 		ventana_chat.getContentPane().add(contenedor_btntxt,BorderLayout.SOUTH);
+		//scroll = new JScrollPane(area_chat);
+
 		
 		TextArea area_chat_1 = new TextArea();
 		ventana_chat.getContentPane().add(area_chat_1, BorderLayout.CENTER);
@@ -48,9 +68,65 @@ public class VentanaCliente  {
 		ventana_chat.setResizable(false);
 		ventana_chat.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	
+		Thread principal =  new Thread(new Runnable() {
+			public void run() {
+				try {
+					socket = new Socket("local host",9000);
+ 						leer();
+						escribir();
+					
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+		});
+		principal.start();
 	}
 	
-	
+	public void leer() {
+		Thread leer_hilo = new Thread(new Runnable(){
+			
+			public void run() {
+				try {
+					lector = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+					while(true) {
+						String mensaje_recibido = lector.readLine();
+						area_chat.append("Servidor dice:  " + mensaje_recibido+"\n");
+					}
+				}catch(Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+		});
+		leer_hilo.start();
+		
+		
+		
+	}
+
+	public void escribir() {
+		Thread escribir_hilo = new Thread(new Runnable() {
+			public void run() {
+				try {
+					escritor = new PrintWriter(socket.getOutputStream(),true);
+					btn_enviar.addActionListener(new ActionListener() {
+						public void actionPerformed(ActionEvent e ) {
+							String enviar_mensaje = txt_mensaje.getText();
+							escritor.println(enviar_mensaje);
+							txt_mensaje.setText("");
+
+						}
+					});
+					
+				}catch (Exception ex) {
+					ex.printStackTrace();
+					
+				}
+			}
+		}); 
+		escribir_hilo.start();
+		
+	}
 	
 	
 	
--- skapaProyect/src/skapaProyect/VentanaServidor.java
+++ skapaProyect/src/skapaProyect/VentanaServidor.java
@@ -2,6 +2,9 @@ package skapaProyect;
 
 import javax.swing.*;
 import java.awt.*;
+import java.net.*;
+import java.io.*;
+import java.awt.event.*;
 
 
 public class VentanaServidor  {
@@ -12,6 +15,11 @@ public class VentanaServidor  {
 	JTextArea area_chat = null;
 	JPanel contenedor_areachat = null;
 	JPanel contenedor_btntxt = null;
+	//JScrollPane scroll = null;
+	ServerSocket servidor = null;
+	Socket socket = null;
+	BufferedReader lector = null;
+	PrintWriter escritor=null;
 
 public VentanaServidor() {
 	
@@ -24,6 +32,7 @@ public void hacerInterfaz() {
 	txt_mensaje = new JTextField(4);
 	contenedor_areachat = new JPanel();
 	contenedor_areachat.setLayout(new GridLayout(1,1));
+	//contenedor_areachat.add(scroll);
 	contenedor_btntxt = new JPanel();
 	contenedor_btntxt.setLayout(new GridLayout(1,2));
 	contenedor_btntxt.add(txt_mensaje);
@@ -31,14 +40,75 @@ public void hacerInterfaz() {
 	ventana_chat.getContentPane().setLayout(new BorderLayout());
 	ventana_chat.getContentPane().add(contenedor_areachat,BorderLayout.NORTH);
 	ventana_chat.getContentPane().add(contenedor_btntxt,BorderLayout.SOUTH);
-	
+	//scroll = new JScrollPane(area_chat);
 	TextArea area_chat_1 = new TextArea();
 	ventana_chat.getContentPane().add(area_chat_1, BorderLayout.CENTER);
 	ventana_chat.setSize(300,200);
 	ventana_chat.setVisible(true);
 	ventana_chat.setResizable(false);
 	ventana_chat.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
+	
+	Thread principal =  new Thread(new Runnable() {
+		public void run() {
+			try {
+				servidor = new ServerSocket(9000);
+				while(true) {
+					socket = servidor.accept();
+					leer();
+					escribir();
+				}
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+	});
+	principal.start();
 
+}
+
+public void leer() {
+	Thread leer_hilo = new Thread(new Runnable(){
+		
+		public void run() {
+			try {
+				lector = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				while(true) {
+					String mensaje_recibido = lector.readLine();
+					area_chat.append("Cliente dice:  " + mensaje_recibido+"\n");
+				}
+			}catch(Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+	});
+	leer_hilo.start();
+	
+	
+	
+}
+
+public void escribir() {
+	Thread escribir_hilo = new Thread(new Runnable() {
+		public void run() {
+			try {
+				escritor = new PrintWriter(socket.getOutputStream(),true);
+				btn_enviar.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e ) {
+						String enviar_mensaje = txt_mensaje.getText();
+						escritor.println(enviar_mensaje);
+						txt_mensaje.setText("");
+					}
+				});
+				
+			}catch (Exception ex) {
+				ex.printStackTrace();
+				
+			}
+		}
+	}); 
+	escribir_hilo.start();
+	
 }
 public static void main(String[] args) {
 	
