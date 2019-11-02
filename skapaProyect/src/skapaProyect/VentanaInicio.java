e7bff96573e350bb693058e0e782f0d00c571542
aitor <aitor@DESKTOP-3BC4VJ0>
2019-11-02 13:13:17 +0100

    Rediseño VentanaInicio

--- skapaProyect/src/skapaProyect/VentanaInicio.java
+++ skapaProyect/src/skapaProyect/VentanaInicio.java
@@ -30,10 +30,14 @@ import javax.swing.JLabel;
 import javax.swing.JSeparator;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
+import java.awt.SystemColor;
+import java.awt.Font;
 
 public class VentanaInicio extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -57,7 +61,7 @@ public class VentanaInicio extends JFrame {
 	public VentanaInicio() {
 		setTitle("VentanaInicio");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 349, 559);
+		setBounds(100, 100, 1308, 735);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(135, 206, 250));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -66,51 +70,66 @@ public class VentanaInicio extends JFrame {
 		
 		JComboBox comboBoxCategoria = new JComboBox();
 		comboBoxCategoria.setModel(new DefaultComboBoxModel(new String[] {"Cocina", "Videojuegos", "Hogar", "Juguetes", "Libreria", "Seguridad", "Codigos", "Prendas", "Informatica", "Comida"}));
-		comboBoxCategoria.setBounds(160, 77, 146, 20);
+		comboBoxCategoria.setBounds(160, 77, 181, 20);
 		contentPane.add(comboBoxCategoria);
 		
 		JComboBox comboBoxProvincia = new JComboBox();
 		comboBoxProvincia.setModel(new DefaultComboBoxModel(new String[] {"Coru\u00F1a", "Pontevedra", "Lugo", "Orense", "Asturias", "Cantabria", "Bizkaia", "Gipuzkoa", "Alaba", "Navarra", "Huesca", "Zaragoza", "L\u00E9rida", "Girona", "Barcelona", "Tarragona", "Teruel", "La Rioja", "Le\u00F3n", "Palencia", "Burgos", "Soria", "Segovia", "Valladolid", "Zamora", "Salamanca", "\u00C1vila", "Madrid", "Guadalajara", "Cuenca", "Albacete", "Toledo", "Ciudad Real", "Castell\u00F3n", "Valencia", "Alicante", "C\u00E1ceres", "Badajoz", "Murcia", "Huelva", "Sevilla", "C\u00F3rdoba", "Ja\u00E9n", "C\u00E1diz", "M\u00E1laga", "Granada", "Almeria", "Islas Baleares", "Islas Canarias"}));
-		comboBoxProvincia.setBounds(160, 102, 146, 20);
+		comboBoxProvincia.setBounds(160, 117, 181, 20);
 		contentPane.add(comboBoxProvincia);
 		
-		JComboBox comboBoxPrecio1 = new JComboBox();
-		comboBoxPrecio1.setModel(new DefaultComboBoxModel(new String[] {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90", "91", "92", "93", "94", "95", "96", "97", "98", "99", "100"}));
-		comboBoxPrecio1.setBounds(160, 127, 59, 20);
-		contentPane.add(comboBoxPrecio1); 
-		
-		JComboBox comboBoxPrecio2 = new JComboBox();
-		comboBoxPrecio2.setModel(new DefaultComboBoxModel(new String[] {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90", "91", "92", "93", "94", "95", "96", "97", "98", "99", "100"}));
-		comboBoxPrecio2.setBounds(237, 127, 59, 20);
-		contentPane.add(comboBoxPrecio2);
-		
 		JEditorPane buscador = new JEditorPane();
-		buscador.setBounds(87, 41, 225, 20);
+		buscador.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		buscador.setBounds(15, 25, 642, 36);
 		contentPane.add(buscador);
 		
 		JLabel lblCategoria = new JLabel("Categoria:");
-		lblCategoria.setBounds(20, 77, 80, 20);
+		lblCategoria.setBounds(15, 77, 80, 20);
 		contentPane.add(lblCategoria);
 		
 		JLabel lblProvincia = new JLabel("Provincias\r\n e Islas:");
-		lblProvincia.setBounds(20, 94, 132, 36);
+		lblProvincia.setBounds(13, 109, 132, 36);
 		contentPane.add(lblProvincia);
 		
 		JLabel lblPrecio = new JLabel("Precio:");
-		lblPrecio.setBounds(20, 127, 59, 20);
+		lblPrecio.setBounds(20, 153, 59, 20);
 		contentPane.add(lblPrecio);
 		
 		JButton btnPerfil = new JButton("Perfil");
-		btnPerfil.setBounds(10, 16, 115, 29);
+		btnPerfil.setBounds(1156, 16, 115, 29);
 		contentPane.add(btnPerfil);
 		
 		JPanel panelAnuncios = new JPanel();
 		panelAnuncios.setBackground(new Color(135, 206, 250));
-		panelAnuncios.setBounds(15, 189, 297, 298);
+		panelAnuncios.setBounds(15, 201, 1256, 431);
 		contentPane.add(panelAnuncios);
 		panelAnuncios.setLayout(null);
 		
-	
+		JButton btnNewButton = new JButton("BUSCAR");
+		btnNewButton.setBackground(new Color(240, 255, 255));
+		btnNewButton.setBounds(686, 25, 132, 36);
+		contentPane.add(btnNewButton);
+		
+		JLabel lblEntre = new JLabel("entre");
+		lblEntre.setBounds(86, 153, 69, 20);
+		contentPane.add(lblEntre);
+		
+		JLabel lblY = new JLabel("y");
+		lblY.setBounds(261, 153, 26, 20);
+		contentPane.add(lblY);
+		
+		textField = new JTextField();
+		textField.setBounds(146, 153, 100, 26);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(287, 153, 100, 26);
+		contentPane.add(textField_1);
+		
+		
+		
 		
 		try {
 			Class.forName("org.sqlite.JDBC");
@@ -137,7 +156,7 @@ public class VentanaInicio extends JFrame {
 			
 				JPanel panelAnuncio = new JPanel();
 				panelAnuncio.setBackground(new Color(255,255,255));
-				panelAnuncio.setBounds(0, cont, 297, 76);
+				panelAnuncio.setBounds(0, cont, 1250, 90);
 				panelAnuncios.add(panelAnuncio);
 				panelAnuncio.setLayout(null);
 				
@@ -145,11 +164,29 @@ public class VentanaInicio extends JFrame {
 				tituloAnuncio.setBounds(15, 16, 117, 20);
 				panelAnuncio.add(tituloAnuncio);
 				
-				JLabel precioAnuncio = new JLabel("Precio: " + precio);
+				JLabel precioAnuncio = new JLabel("PRECIO: " + precio + " €");
 				precioAnuncio.setBounds(15, 47, 96, 20);
 				panelAnuncio.add(precioAnuncio);
 				
-				cont = cont + 80;
+				JLabel lblNewLabel = new JLabel(descripcion);
+				lblNewLabel.setBounds(260, 0, 688, 47);
+				panelAnuncio.add(lblNewLabel);
+				
+				JLabel labelImagen = new JLabel("[IMAGEN]");
+				labelImagen.setBackground(SystemColor.activeCaptionBorder);
+				labelImagen.setBounds(1000, 0, 87, 76);
+				panelAnuncio.add(labelImagen);
+				
+				JButton btnContactar = new JButton("Contactar");
+				btnContactar.setBounds(1142, 16, 99, 23);
+				panelAnuncio.add(btnContactar);
+				
+				JButton btninfo = new JButton("+INFO");
+				btninfo.setBounds(1142, 44, 99, 23);
+				panelAnuncio.add(btninfo);
+				
+				
+				cont = cont + 95;
 			}
 			
 			stmt.close();
