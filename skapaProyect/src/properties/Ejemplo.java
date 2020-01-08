4fda8c13efccb2d5148252aff3896300809741b5
Gaizka <Gaizka@LAPTOP-ONLMKIV7>
2020-01-08 19:15:07 +0100

    Ejemplos de properties(falta mejorarla)

---
+++ skapaProyect/src/properties/Ejemplo.java
@@ -0,0 +1,35 @@
+package properties;
+
+import java.io.*;
+import java.util.*;
+ 
+public class Ejemplo {
+    public static void main(String[] args) throws IOException{
+		Properties prop = new Properties();
+		InputStream is = null;
+		
+		try {
+			is = new FileInputStream("c:\\ejemplo.properties");
+			prop.load(is);
+		} catch(IOException e) {
+			System.out.println(e.toString());
+		}
+ 
+		// Acceder a las propiedades por su nombre
+		System.out.println("Propiedades por nombre:");
+		System.out.println("-----------------------");
+		System.out.println(prop.getProperty("servidor.nombre"));
+		System.out.println(prop.getProperty("servidor.password"));
+		System.out.println(prop.getProperty("servidor.usuario"));
+		
+		// Recorrer todas sin conocer los nombres de las propiedades
+		System.out.println("Recorrer todas las propiedades:");
+		System.out.println("-------------------------------");
+ 
+		for (Enumeration e = prop.keys(); e.hasMoreElements() ; ) {
+			// Obtenemos el objeto
+			Object obj = e.nextElement();
+			System.out.println(obj + ": " + prop.getProperty(obj.toString()));
+		}
+	}
+}
